11984875554392e284dfd223b98b0d1212230ef7
zecaafonso <80847211+zecaafonso@users.noreply.github.com>
2025-11-26 15:46:54 +0000

    a

--- EDA.py
+++ EDA.py
@@ -63,22 +63,6 @@ print(missing_data[missing_data > 0])
 print(f"Total de valores nulos: {missing_data_total}")
 print(f"Total de linhas no dataset: {df_clean.shape[0]}")
 
-#so copiei e colei do gpt o boxplot
-print("VISUALIZACAO DE OUTLIERS (BOXPLOTS):")
-fig, axes = plt.subplots(1, 3, figsize=(15, 5))
-
-sns.boxplot(data=df_clean, y='rating', ax=axes[0])
-axes[0].set_title('Outliers - Rating')
-
-sns.boxplot(data=df_clean, y='votes', ax=axes[1])
-axes[1].set_title('Outliers - Votes')
-
-sns.boxplot(data=df_clean, y='year', ax=axes[2])
-axes[2].set_title('Outliers - Year')
-
-plt.tight_layout()
-plt.show()
-
 # Método IQR para detectar outliers
 def detect_outliers_iqr(column):
     Q1 = column.quantile(0.25)
@@ -94,21 +78,5 @@ for col in ['rating', 'votes', 'year']:
     outliers = detect_outliers_iqr(df_clean[col])
     print(f"{col}: {len(outliers)} outliers")
 
-# Normalização Min-Max(FICA COMENTADO POR AGORA PORQUE E CAPAZ DE NAO SE USAR)
-""" print("\n" + "="*50)
-print("NoRMALIZAÇÃO")
-scaler = MinMaxScaler()
-df_clean[['rating_norm', 'votes_norm']] = scaler.fit_transform(df_clean[['rating', 'votes']]) """
 
-# Codificação de variáveis
-""" certificate_dummies = pd.get_dummies(df_clean['certificate'], prefix='cert')
-df_clean = pd.concat([df_clean, certificate_dummies], axis=1)
-
-print("Variáveis categóricas codificadas")
-
-# Variáveis derivadas
-df_clean['engagement_score'] = df_clean['rating'] * np.log1p(df_clean['votes'])
-df_clean['is_modern'] = (df_clean['year'] > 2000).astype(int) #nao sei se e vamos usar
-
-print(df_clean[['rating', 'rating_norm', 'votes', 'votes_norm']].head()) """
 
